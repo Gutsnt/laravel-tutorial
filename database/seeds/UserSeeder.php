2feeefc7b880437273b5082a981f7212c02ef741
PlayerThatPreaches <charcristo20@gmail.com>
2020-09-09 19:09:33 -0400

    Update

---
+++ database/seeds/UserSeeder.php
@@ -0,0 +1,20 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('users')->insert([
+          'name' => 'Dulio Palacios',
+           'email' => 'deulios.net',
+           'password' => bcrypt('laravel'),
+         ]);
+    }
+}
