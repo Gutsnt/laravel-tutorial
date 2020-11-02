483fe5380002aab65fa3770c20a5bfb1c00ead08
PlayerThatPreaches <charcristo20@gmail.com>
2020-11-01 20:45:48 -0400

    forms

--- resources/views/users/create.blade.php
+++ resources/views/users/create.blade.php
@@ -9,6 +9,16 @@
         <form method="POST" action="{{ url('usuarios')}}">
             {{ csrf_field() }}
 
+          <label for="name">Nombre:</label>
+          <input type="text" name="name" id="name" placeholder="Juan perez">
+
+           <br>
+          <label for="email">Email:</label>
+          <input type="email" name="email" id="email" placeholder="juan@example.com">
+           <br>
+          <label for="password">Password:</label>
+          <input type="password" name="password" id="password" placeholder="mayor a 6 caracteres">
+
           <button type="submit">Crear Usuario</button>
 
         </form>
