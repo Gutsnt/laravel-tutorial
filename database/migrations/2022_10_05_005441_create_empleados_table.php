6a6842563a0e21801b1f7bce1825e7418989f73d
Charlie Mendoza <charlie.mendoza1337@gmail.com>
2022-10-05 04:53:36 -0400

    "Migrations"

--- database/migrations/2022_10_05_005441_create_empleados_table.php
+++ database/migrations/2022_10_05_005441_create_empleados_table.php
@@ -13,13 +13,15 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('empleados', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
+
+            $table->String('Nombre');
+            $table->String('ApellidoPaterno');
+            $table->String('ApellidoMaterno');
+            $table->String('Correo');
+            $table->String('Foto');
+
             $table->timestamps();
         });
     }
@@ -31,6 +33,6 @@ return new class extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('empleados');
     }
 };
