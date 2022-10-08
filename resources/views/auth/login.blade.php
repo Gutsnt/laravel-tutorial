f526dc511211f7c4ef4d2ad8a3f97d85187993eb
Charlie Mendoza <charlie.mendoza1337@gmail.com>
2022-10-08 04:05:58 -0400

    "a-little-close"

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -3,19 +3,24 @@
 @section('content')
 <div class="container">
     <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Login') }}</div>
-                <img src="logo.jpeg" />
-                <div class="card-body">
+        <div class="col-md-8 row justify-content-center">
+            <div class="container">
+              <img src="logo.jpeg" class="mx-auto d-block" width="50" height="50" />
+            </div>
+            <div class="border card shadow-lg p-3 mb-5 bg-body rounded "  style="max-width: 500px;">
+                <div class="card-body ">
+                    <div class="h5" >
+                        <strong>{{ __('Iniciar sesión') }}</strong>
+                        <p><small>Continuar a Worki-Fact</small></p>
+                    </div>
                     <form method="POST" action="{{ route('login') }}">
                         @csrf
 
                         <div class="row mb-3">
-                            <label for="email" class="col-md-4 col-form-label text-md-end">{{ __('Email Address') }}</label>
+                            <label for="email" class="form-label">{{ __('Usuario') }}</label>
 
                             <div class="col-md-6">
-                                <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
+                                <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" placeholder="tucorreo/nickname/telefono" autofocus>
 
                                 @error('email')
                                     <span class="invalid-feedback" role="alert">
@@ -26,10 +31,10 @@
                         </div>
 
                         <div class="row mb-3">
-                            <label for="password" class="col-md-4 col-form-label text-md-end">{{ __('Password') }}</label>
+                            <label for="password" class="form-label">{{ __('Contraseña') }}</label>
 
                             <div class="col-md-6">
-                                <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="current-password">
+                                <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password"  placeholder="************"  required autocomplete="current-password">
 
                                 @error('password')
                                     <span class="invalid-feedback" role="alert">
@@ -38,30 +43,25 @@
                                 @enderror
                             </div>
                         </div>
-
-                        <div class="row mb-3">
-                            <div class="col-md-6 offset-md-4">
-                                <div class="form-check">
-                                    <input class="form-check-input" type="checkbox" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}>
-
-                                    <label class="form-check-label" for="remember">
-                                        {{ __('Remember Me') }}
-                                    </label>
-                                </div>
-                            </div>
+                        <div class="">
+                        @if (Route::has('password.request'))
+                              <a class="btn btn-link link-dark" href="{{ route('password.request') }}">
+                                {{ __('¿Olvido su Contraseña?') }}
+                              </a>
+                        @endif
                         </div>
-
+                        <div class="">
+                        @if (Route::has('password.request'))
+                              <a class="btn btn-link link-dark" href="{{ route('register') }}">
+                                {{ __('¿No tiene una cuenta? Crea una.') }}
+                              </a>
+                        @endif
+                        </div>
                         <div class="row mb-0">
-                            <div class="col-md-8 offset-md-4">
-                                <button type="submit" class="btn btn-primary">
-                                    {{ __('Login') }}
+                            <div class="col-md-8 offset-md-2 ">
+                                <button type="submit" class="btn btn-primary col-11 active">
+                                    <strong>{{ __('INICIAR SESION') }}</strong>
                                 </button>
-
-                                @if (Route::has('password.request'))
-                                    <a class="btn btn-link" href="{{ route('password.request') }}">
-                                        {{ __('Forgot Your Password?') }}
-                                    </a>
-                                @endif
                             </div>
                         </div>
                     </form>
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -37,12 +37,18 @@
                     <ul class="navbar-nav ms-auto">
                         <!-- Authentication Links -->
                         @guest
+                            @if (url()->current() == 'login')
+                                <li class="nav-item">
+                                    <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
+                                </li>
+                            @endif
+                            <!--
                             @if (Route::has('login'))
                                 <li class="nav-item">
                                     <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
                                 </li>
                             @endif
-
+-->
                             @if (Route::has('register'))
                                 <li class="nav-item">
                                     <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
